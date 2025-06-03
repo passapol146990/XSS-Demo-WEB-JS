89714efed76ededc498c053f2f37c4fae1c79a40
Passapol <phasphlsuthathrrm976@gmail.com>
2025-06-03 13:19:36 +0700

    setup

--- src/route.js
+++ src/route.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const db = require('./db');
 
 router.get('/', async (req, res) => {
-    const sql = 'SELECT * FROM message ORDER BY id DESC';
+    const sql = 'SELECT * FROM messages ORDER BY id DESC';
     const messages = await db.query(sql);
     res.render('index', { messages:messages[0] });
 });
 
 router.get('/api/messages', async (req, res) => {
-    const sql = 'SELECT * FROM message ORDER BY id DESC';
+    const sql = 'SELECT * FROM messages ORDER BY id DESC';
     const messages = await db.query(sql);
     res.json(messages[0]);
 });
@@ -21,7 +21,7 @@ router.get('/create', (req, res) => {
 router.post('/create', (req, res) => {
     const { message } = req.body;
     if (message) {
-        db.query('INSERT INTO message (message) VALUES (?)', [message]);
+        db.query('INSERT INTO messages (message) VALUES (?)', [message]);
     };
     res.redirect('/');
 });
